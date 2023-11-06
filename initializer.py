0a4e94f08b6ad6f5071780d00abf8323160266de
morrisettjohn <60852062+morrisettjohn@users.noreply.github.com>
2023-11-06 16:41:03 -0500

    Cleaned up functions

---
+++ initializer.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+
+def initializer(num_part: int, num_dim: int, alpha: np.float64, upper_bound: np.ndarray, lower_bound: np.ndarray):
+    """ Initialization function for the PSO algorithm.
+    num_part:  Number of particles
+    num_dim: Number of dimensions
+    alpha:  parameter for velocity max
+    upper_bound:  Upper bounds of the domain of the problem
+    lower_bound:  Lower bounds of the domain of the problem
+    """
+
+    # Randomly initialize the positions of each of the particles
+    pos_matrix = _x_initializer(num_dim=num_dim, num_part=num_part, upper_bound=upper_bound, lower_bound=lower_bound)
+
+    # Randomly assign velocities to each of the particles
+    vel_matrix, v_max = _v_initializer(num_dim=num_dim, num_part=num_part, upper_bound=upper_bound, lower_bound=lower_bound, alpha = alpha)
+
+    # The distances row contians the distances for each particle's p_best.  It is used to keep track of
+    # Results so no recalculation is needed.
+    distances_row = np.ones((1, num_part))
+    distances_row *= np.finfo(np.float64).max
+
+    # Let the personal best be the current position.
+    p_best = np.vstack((pos_matrix, distances_row))
+
+    #TODO:  make g_best its own function
+    g_best = np.random.rand(num_dim, 1)
+
+    return pos_matrix, vel_matrix, p_best, g_best, v_max
+
+def _x_initializer(num_dim: int, num_part: int, upper_bound: np.ndarray, lower_bound: np.ndarray):
+    scalingfactor = upper_bound - lower_bound
+
+    pos_matrix = np.random.rand(num_dim, num_part)
+
+    pos_matrix[ : ] *= scalingfactor[ : , np.newaxis]
+    pos_matrix[ : ] += lower_bound[ : , np.newaxis]
+
+    return pos_matrix
+
+def _v_initializer(num_dim: int, num_part: int, upper_bound: np.ndarray, lower_bound: np.ndarray, alpha: float):
+    if alpha < 0 or alpha >= 1:
+        raise Exception("Whomp whomp")
+    
+    v_max = alpha*(upper_bound - lower_bound)
+
+    scalingfactor = 2*v_max
+
+    vel_matrix = np.random.rand(num_dim, num_part)
+
+    vel_matrix[ : ] *= scalingfactor[ : , np.newaxis]
+    vel_matrix[ : ] -= (v_max)[ : , np.newaxis]
+
+    return vel_matrix, v_max
--- velocity.py
+++ velocity.py
@@ -1,5 +1,12 @@
 import numpy as np
 import random
+import initializer as ini
+
+def update_p_best(pos_matrix: np.ndarray, p_best: np.ndarray, function):
+    pass
+
+def good_learning_parameters(w: float, c1: float, c2: float):
+    return w < 1 and w > 0.5*(c1+c2)
 
 num_dim = 4
 num_part = 5
@@ -10,52 +17,6 @@ lower_bound = np.array([1, 2, 3, 4])
 
 alpha = 0.3
 
-def x_initializer():
-    #in a real problem, we'll have to define each scaling factor before we calculate ?
-    scalingfactor = upper_bound - lower_bound
-
-    pos_matrix = np.random.rand(num_dim, num_part)
-
-    pos_matrix[ : ] *= scalingfactor[ : , np.newaxis]
-    pos_matrix[ : ] += lower_bound[ : , np.newaxis]
-
-    return pos_matrix
-
-def v_initializer(alpha: float):
-    if alpha < 0 or alpha >= 1:
-        raise Exception("Whomp whomp")
-    
-    vmax = upper_bound - lower_bound
-
-    scalingfactor = 2*vmax
-
-    vel_matrix = np.random.rand(num_dim, num_part)
-
-    vel_matrix[ : ] *= scalingfactor[ : , np.newaxis]
-    vel_matrix[ : ] += alpha*(-1*vmax)[ : , np.newaxis]
-
-    return vel_matrix, vmax
-
-def initializer():
-    pos_matrix = x_initializer()
-    vel_matrix, vmax = v_initializer(alpha = alpha)
-    pbest = x_initializer()
-    gbest = np.random.rand(num_dim, 1)
-
-initializer()
-
-
-    
-
-
-def good_learning_parameters(w: float, c1: float, c2: float):
-    if w < 1 and w > 0.5*(c1+c2):
-        return True
-    return False
-
-if not good_learning_parameters(w, c1, c2):
-    raise Exception("Bad parameters")
-
 #Learning parameters:
 #Weight part of inertia compoennet
 w = 0.7 #velocity, Randomized from 0 - 1
@@ -64,13 +25,15 @@ w = 0.7 #velocity, Randomized from 0 - 1
 c1 = 0.3 #Random numbe from [0 - 2), fixed throughout the function,
 c2 = 0.4 #Random number from [0- 2), fixed throughout the function
 
+if not good_learning_parameters(w, c1, c2):
+    raise Exception("Bad parameters")
+
 k = None #Step increment
 
 #Randomize velocity vector
-g_best = np.random.rand(num_dim, 1)
-p_best = initializer()
-x_pos = initializer()
-v_part = initializer()
+
+pos_matrix, vel_matrix, p_best, g_best, v_max = ini.initializer(num_part=num_part, num_dim=num_dim, alpha=alpha, upper_bound=upper_bound,
+                                                                lower_bound=lower_bound)
 
 def update_velocity(v_part, x_pos, g_best, p_best, w, c1, c2):
 #Randomness variables
@@ -84,9 +47,9 @@ def update_velocity(v_part, x_pos, g_best, p_best, w, c1, c2):
 
     for i in range(num_part):
         # The three parts below are, Inertia, cognitive path, social/global part
-        v_part[:, i] = (w*v_part[:, i]) + r1*c1*(x_pos[:, i] - p_best[:, i]) + r2*c2*(x_pos[:, i] - g_best[:, 0])
+        v_part[:, i] = (w*v_part[:, i]) + r1*c1*(x_pos[:, i] - p_best[:-1, i]) + r2*c2*(x_pos[:, i] - g_best[:, 0])
 
     print("velocity is now:")
     print(v_part)
 
-update_velocity(v_part=v_part, x_pos=x_pos, g_best=g_best, p_best=p_best, w=w, c1=c1, c2=c2)
+update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=w, c1=c1, c2=c2)
