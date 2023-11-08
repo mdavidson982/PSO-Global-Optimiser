a5704f722337435cfbed800d0b49fd1bfc66f76a
mdavidson982 <mdavidson982@g.rwu.edu>
2023-11-07 20:55:32 -0500

    Moved Update Velocity

--- update.py
+++ update.py
@@ -10,3 +10,19 @@ def update_p_best(pos_matrix: np.ndarray, past_p_best: np.ndarray, function) -> 
 def update_g_best(p_best: np.ndarray) -> np.ndarray:
     # Since g_best should always be in p_best, return the min of p_best.
     return p_best[:, np.argmin(p_best[-1, :])].copy()
+
+
+def update_velocity(v_part: np.ndarray, x_pos: np.ndarray, g_best: np.ndarray, 
+                    p_best: np.ndarray, w: np.float64, c1: np.float64, c2: np.float64):
+#Randomness variables
+    r1 = np.random.rand()
+    r2 = np.random.rand()
+
+    print(f"r1 is: {r1}, r2 is {r2}")
+
+    print("velocity was:")
+    print(v_part)
+    v_part = v_part*w + r1*c1*(x_pos-p_best[:-1]) + r2*c2*(x_pos-g_best[:-1, np.newaxis])
+
+    print("velocity is now:")
+    print(v_part)
--- velocity.py
+++ velocity.py
@@ -1,6 +1,7 @@
 import numpy as np
 import initializer as ini
 import parameters as p
+import update as up
 
 def good_learning_parameters(w: float, c1: float, c2: float):
     return w < 1 and w > 0.5*(c1+c2)
@@ -14,19 +15,5 @@ if not good_learning_parameters(p.W, p.C1, p.C2):
 pos_matrix, vel_matrix, p_best, g_best, v_max = ini.initializer(num_part=p.NUM_PART, num_dim=p.NUM_DIM, alpha=p.ALPHA, upper_bound=p.UPPER_BOUND,
                                                                 lower_bound=p.LOWER_BOUND)
 
-def update_velocity(v_part: np.ndarray, x_pos: np.ndarray, g_best: np.ndarray, 
-                    p_best: np.ndarray, w: np.float64, c1: np.float64, c2: np.float64):
-#Randomness variables
-    r1 = np.random.rand()
-    r2 = np.random.rand()
 
-    print(f"r1 is: {r1}, r2 is {r2}")
-
-    print("velocity was:")
-    print(v_part)
-    v_part = v_part*w + r1*c1*(x_pos-p_best[:-1]) + r2*c2*(x_pos-g_best[:-1, np.newaxis])
-
-    print("velocity is now:")
-    print(v_part)
-
-update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=p.W, c1=p.C1, c2=p.C2)
+up.update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=p.W, c1=p.C1, c2=p.C2)
