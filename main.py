138ac297eb04c9bebf690dcfdc31f5e8684fef11
Lauren Moulaison <lmoulaison975@g.rwu.edu>
2023-11-09 13:36:10 -0500

    Added Comments main class

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ pos_matrix, vel_matrix, p_best, g_best, v_max = ini.initializer(num_part=p.NUM_P
 
 
 up.update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=p.W, c1=p.C1, c2=p.C2)
+
+
+#Here we will place the MPSO function. To achieve this we will loop through the velocity update, position update,
+#p_best, and g_best functions until we get a satisfactory answer, and loop under the number of times defined in
+#max iteration var
