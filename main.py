8ea4f08df75a35388ab96d836f2dcc4bcd5fabcc
mdavidson982 <mdavidson982@g.rwu.edu>
2023-11-09 13:34:03 -0500

    Added Max Iterations variable

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import initializer as ini
 import parameters as p
 import update as up
 
+
+
 def good_learning_parameters(w: float, c1: float, c2: float):
     return w < 1 and w > 0.5*(c1+c2)
 
@@ -17,3 +19,4 @@ pos_matrix, vel_matrix, p_best, g_best, v_max = ini.initializer(num_part=p.NUM_P
 
 
 up.update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=p.W, c1=p.C1, c2=p.C2)
+
