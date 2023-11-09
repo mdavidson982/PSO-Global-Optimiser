c9fc1b5f24aa94c5900a9ce9a9d75f2376f19979
mdavidson982 <mdavidson982@g.rwu.edu>
2023-11-09 13:55:11 -0500

    Added documentation to main. Listed Initialization

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ if not good_learning_parameters(p.W, p.C1, p.C2):
     raise Exception("Bad parameters")
 
 #initialization variables
+#calls the initialization function in the initializer.py. 
+#Returns: pos_matrix, an ndarray that keep the position of each particle (Initialized with random values for each dimmension)
+#         vel_matrix, an ndarray that keeps the velocity for each particle (Initialized with random values for each dimmension)
+#         p_best, an ndarray that keeps the personal minimum for each particle in each dimmension
+#         g_best, an ndarray that keeps the global minimum between all particles in each dimmension
+#         v_max, float based on the size of the area, this is the max velocity each particle can move 
 pos_matrix, vel_matrix, p_best, g_best, v_max = ini.initializer(num_part=p.NUM_PART, num_dim=p.NUM_DIM, alpha=p.ALPHA, upper_bound=p.UPPER_BOUND,
                                                                 lower_bound=p.LOWER_BOUND)
 
@@ -24,3 +30,5 @@ up.update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_
 #Here we will place the MPSO function. To achieve this we will loop through the velocity update, position update,
 #p_best, and g_best functions until we get a satisfactory answer, and loop under the number of times defined in
 #max iteration var
+
+for p.MAX_ITERATIONS:
