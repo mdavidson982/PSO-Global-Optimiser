4b165b5d9d2329b00fef0d53d06fa4ccc0946652
Lauren Moulaison <lmoulaison975@g.rwu.edu>
2023-11-09 14:14:21 -0500

    Added Loop to Main

--- main.py
+++ main.py
@@ -25,11 +25,17 @@ pos_matrix, vel_matrix, p_best, g_best, v_max = ini.initializer(num_part=p.NUM_P
                                                                 lower_bound=p.LOWER_BOUND)
 
 
-up.update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=p.W, c1=p.C1, c2=p.C2)
+
 
 
 #Here we will place the MPSO function. To achieve this we will loop through the velocity update, position update,
 #p_best, and g_best functions until we get a satisfactory answer, and loop under the number of times defined in
 #max iteration var
 
-for _ in range(p.MAX_ITERATIONS):
+
+for _ in range (p.MAX_ITERATIONS):
+    up.update_velocity(v_part=vel_matrix, x_pos=pos_matrix, g_best=g_best, p_best=p_best, w=p.W, c1=p.C1, c2=p.C2)
+    up.update_position(x_pos=pos_matrix, v_part=vel_matrix)
+    up.update_p_best(pos_matrix: np.ndarray, past_p_best: np.ndarray, update_p_best())
+    up.update_g_best(p_best=p_best)
+
