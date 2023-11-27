f73bb4455ce71a2d546d2b4f2a1d84f884deb6fe
dle245 <120441525+dle245@users.noreply.github.com>
2023-11-26 23:18:49 -0500

    Update update.py

--- update.py
+++ update.py
@@ -9,12 +9,12 @@ def _Default(array: np.ndarray[p.DTYPE]) -> p.DTYPE:
 
 def update_p_best(pos_matrix: np.ndarray[p.DTYPE], past_p_best: np.ndarray[p.DTYPE], function = _Default) -> np.ndarray[p.DTYPE]:
     """Updates the personal best for each particle in each iteration (if the updated value is smaller than the previous value).
-    This is completed by preforming the optomization function on each particle
+    This is completed by peforming the optimization function on each particle
 
     Pos_matrix: ndarray that represents the current position of each particle
     Past_p_best: represents the previous personal best position for each particle
-    Function: preforms optimization function, currently uses _Default as default input if not provided
-    np.ndarray (return type): The following function returns an ndarray that repersents the updated personal best positions"""
+    Function: peforms optimization function, currently uses _Default as default input if not provided
+    np.ndarray (return type): The following function returns an ndarray that represents the updated personal best positions"""
     results = np.apply_along_axis(function, axis=0, arr=pos_matrix) # Run the function for every particle, and store the result in an array
     evaluated = np.vstack((pos_matrix, results)) # Append the results to the bottom of the position matrix
     mask = past_p_best[-1, :] < evaluated[-1, :] # Boolean mask for every row.  Basically, only update columns if the result is smaller
@@ -22,27 +22,27 @@ def update_p_best(pos_matrix: np.ndarray[p.DTYPE], past_p_best: np.ndarray[p.DTY
 
 def update_g_best(p_best: np.ndarray[p.DTYPE]) -> np.ndarray[p.DTYPE]:
     """Updates the global minimum value found in the p_best for each particle.
-    g_best is determined by selecting the particle with the best minimum in each dimmension"""
+    g_best is determined by selecting the particle with the best minimum in each dimension"""
     # Since g_best should always be in p_best, return the min of p_best.
 
-    #np.argmin(p_best[-1, :]) finds the index of the minmum value in the last row of p_best
+    #np.argmin(p_best[-1, :]) finds the index of the minimum value in the last row of p_best
         #Last row contains the objective function value for each particle (output of optimization function [I'm pretty sure])
     #p_best[:, np.argmin(p_best[-1, :])] selects the entire column corresponding to the index found earlier.
         #Selects the personal best positions of the particle with the best with the best minimum
-    #.copy() ensures that the returned g_best ndarray is a seprate copy and not referenced to the original p_best ndarray
+    #.copy() ensures that the returned g_best ndarray is a separate copy and not referenced to the original p_best ndarray
     return p_best[:, np.argmin(p_best[-1, :])].copy()
 
 
 def update_velocity(v_part: np.ndarray[p.DTYPE], x_pos: np.ndarray[p.DTYPE], g_best: np.ndarray[p.DTYPE], 
                     p_best: np.ndarray[p.DTYPE], w: p.DTYPE, c1: p.DTYPE, c2: p.DTYPE):
-    """Updates the velocity for each particle in each dimmension
-    v_part: ndarray that represents the current velocities for each dimmention of a particle
-    x_pos: ndarray that reperesents the current position of the particle
+    """Updates the velocity for each particle in each dimension
+    v_part: ndarray that represents the current velocities for each dimension of a particle
+    x_pos: ndarray that represents the current position of the particle
     g_best: ndarray for the the global best position found by the particle swarm (see more at update_g_best)
     p_best: ndarray for the personal best position found by each particle (Look at update p_best)
     w: ndarray representing inertia weight for the PSO algorithm
     c1: ndarray representing cognitive (personal) component [Stored in parameters.py]
-    c2: ndarray representing social (global) component [Stored in paramtets.py]"""
+    c2: ndarray representing social (global) component [Stored in parameters.py]"""
     
     
     #Randomness variables. Returns values between 0 and 1.
@@ -55,20 +55,20 @@ def update_velocity(v_part: np.ndarray[p.DTYPE], x_pos: np.ndarray[p.DTYPE], g_b
     #Update Velocity Formula
     #v_part * w: Inertia term. It allows particles to retain some of their previous velocity
     #r1 * c1 * (x_pos - p_best[:-1]): Personal Cognitive Component. Pulls the particle towards the personal best (p_best) position
-    #r2 * c2 * (x_pos - g_best[:-1, np.newaxis]): Global Cognitive Component. Pulls the partivle towards the global best position
+    #r2 * c2 * (x_pos - g_best[:-1, np.newaxis]): Global Cognitive Component. Pulls the particle towards the global best position
     v_part = v_part*w + r1*c1*(x_pos-p_best[:-1]) + r2*c2*(x_pos-g_best[:-1, np.newaxis])
     return v_part
 
 
 def update_position(x_pos: np.ndarray[p.DTYPE], v_part: np.ndarray[p.DTYPE]):
-    """Updates the position of a particle by adding the velocity to the position for each dimmension
+    """Updates the position of a particle by adding the velocity to the position for each dimension
     returns an updated position ndarray"""
     return x_pos + v_part
 
 
 def verify_bounds(upper_bound: np.ndarray[p.DTYPE], lower_bound: np.ndarray[p.DTYPE], matrix: np.ndarray[p.DTYPE]):
 
-    """The following function verifies that the matrix does not exceed the upper or lower bound dimmensions. 
+    """The following function verifies that the matrix does not exceed the upper or lower bound dimensions. 
     Here's an example of constraining both Max and Min:
     
         Original array:
