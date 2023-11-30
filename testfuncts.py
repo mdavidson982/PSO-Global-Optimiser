f0697724d80b17d517f5530ebff8e8ed246746db
mdavidson982 <mdavidson982@g.rwu.edu>
2023-11-29 21:14:56 -0500

    Pull for aTHENA

--- testfuncts.py
+++ testfuncts.py
@@ -6,5 +6,13 @@ def Sphere(array: p.ADTYPE) -> np.float64:
     return np.sum(array ** 2)
 
 #Rosenberg
+def rosenbrock(x):
+    # Calculate the Rosenbrock function value for a given input x
+    return np.sum(100 * (x[:-1] - x[1:]**2)**2 + (1 - x[1:])**2)
 
 #Rastrigin
+def rastrigin(x):
+    # Calculate the Rastrigin function value for a given input x
+    return np.sum(x**2 - 10*(np.cos(2*np.pi*x) + 10))
+
+def 
