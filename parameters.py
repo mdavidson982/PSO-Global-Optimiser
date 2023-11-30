1d027dc02d9a306ef795d11ecb91812fa58140de
mdavidson982 <mdavidson982@g.rwu.edu>
2023-11-29 21:35:01 -0500

    added optimim

--- parameters.py
+++ parameters.py
@@ -17,7 +17,7 @@ TOLERANCE: np.float64 = 10**-6
 #Upper and lower bounds (U-L array).  Bound the domain of the function.
 UPPER_BOUND = np.array([20, 20], dtype=np.float64)
 LOWER_BOUND = np.array([-20, -20], dtype=np.float64)
-OPTIMUM = np.array([10,10], dtype=np.float64)
+OPTIMUM = np.array([10,-10], dtype=np.float64)
 
 #Velocity restrictor [0-1].  
 ALPHA: np.float64 = 0.1
